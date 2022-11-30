8dde3adf918c199dad22d6c3e70658613f615140
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-11-30 16:13:00 +0300

    feat: update key

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,23 +1,21 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import path from "path";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`${path}`);
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
